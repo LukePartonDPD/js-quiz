161037b401d42ca89f6bd04c8f9c805389e2d7a7
LukePartonDPD <luke.parton@dpdgroup.co.uk>
2021-07-05 15:23:37 +0100

    finished question 3

--- src/questions/question3.js
+++ src/questions/question3.js
@@ -24,5 +24,19 @@ exports.questionThree = () => {
 
 const answerThree = (names) => {
   //code here
+  if (names.length == 0){
+    return '';
+  } else if (names.length == 1){
+    return names[0].name;
+  } else if(names.length == 2){
+    return names[0].name + ' & ' + names[1].name;
+  } else {
+    let firstPart = '';
+    for (let i = 0; i <= names.length-3; i++){
+      firstPart = firstPart + names[i].name + ', '
+    }
+    let secondPart = names[names.length-2].name + ' & ' + names[names.length-1].name
+    return firstPart + secondPart;
+  }
   
 }
