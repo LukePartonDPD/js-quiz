b7d6a1e2b17d83cb77eaa24ee7f98e11c31b9ceb
LukePartonDPD <luke.parton@dpdgroup.co.uk>
2021-07-05 15:06:50 +0100

    question one completed

--- src/questions/question1.js
+++ src/questions/question1.js
@@ -10,5 +10,10 @@ exports.questionOne = () => {
 
 const answerOne = (str) => {
   //code here
+  let newString = '';
+  for (let i = str.length-1; i >= 0; i--){
+    newString = newString + str[i];
+  }
+  return newString;
   
 }
