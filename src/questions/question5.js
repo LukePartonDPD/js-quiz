9f3ba8122bec592bf74d8d10409dd55d072f6d11
LukePartonDPD <luke.parton@dpdgroup.co.uk>
2021-07-06 13:09:58 +0100

    made it work

--- src/questions/question5.js
+++ src/questions/question5.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 
-exports.questionFive = () => {
+exports.questionFive = async () => {
   console.log("Question 5: Work are organising a seminar for those who are planning their retirement. In the function answerFive, " +
     "make a HTTP GET request using axios with the url listed below and return a filtered array of employees where their age is equal or greater than 50." +
     "(Hint: axios is a promised based library, consider how to handle this so the data is returned. ");
   console.log("axios docs can be found here: https://github.com/axios/axios, guide can be found here: https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/");
-  console.log( answerFive());
+  console.log(await answerFive());
 }
 
 const answerFive = () => {
@@ -14,10 +14,6 @@ const answerFive = () => {
   //code here
   return axios.get(url)
     .then(response => {return getFilteredEmployees(response.data);});
-
-  //return filteredEmployeesPromise;
-
-  
 }
 
 
