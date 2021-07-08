ee8442cbe9d84340fca971161489bc7af9e72c57
LukePartonDPD <luke.parton@dpdgroup.co.uk>
2021-07-08 09:50:08 +0100

    refactored question 2

--- src/questions/question2.js
+++ src/questions/question2.js
@@ -11,14 +11,19 @@ exports.questionTwo = () => {
 
 const answerTwo = (str) => {
   //code here
+  // const vowels = ['a', 'e', 'i', 'o', 'u'];
+  // let vowelCount = 0;
+  // const lowerStr = str.toLowerCase();
+  // for (let i = 0; i < lowerStr.length; i++){
+  //   if (vowels.includes(lowerStr[i])){
+  //     vowelCount++;
+  //   }
+  // }
+  // return vowelCount;
+
   const vowels = ['a', 'e', 'i', 'o', 'u'];
-  let vowelCount = 0;
-  const lowerStr = str.toLowerCase();
-  for (let i = 0; i < lowerStr.length; i++){
-    if (vowels.includes(lowerStr[i])){
-      vowelCount++;
-    }
-  }
-  return vowelCount;
+  return str.toLowerCase()
+    .split('')
+    .reduce((vowelCount, currChar) => {return vowelCount + (vowels.includes(currChar) ? 1 : 0)}, 0)
   
 }
